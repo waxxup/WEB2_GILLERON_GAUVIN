b4973b5c00e5238afbd04303bace29d55dd33692
momolafripouille <Nicolas-g7@hotmail.fr>
2016-03-11 18:33:24 +0100

    duxième test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">cacahuete 5</div>
+                <h2>yutaeuyateuyaetuyteau</h2>
             </div>
         </div>
     </body>
