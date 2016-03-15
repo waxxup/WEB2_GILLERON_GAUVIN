0b1f9314ef1eeee799978758eae6ebedf42fa300
Sébastien Gilleron <seb.gilleron@hotmail.fr>
2016-03-15 15:12:12 +0100

    début page profil modifiable

---
+++ resources/views/Profil/editProfil.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+                    <div class="panel-heading">Modification de votre profil</div>
+
+                    <div class="panel-body">
+                        Your Application's Landing Page.
+                    </div>
+
+@endsection
