c48a88887f8bc3e65879098067b4edebce6cc77c
Sébastien Gilleron <seb.gilleron@hotmail.fr>
2016-03-17 16:58:05 +0100

    début création commentaires

--- database/migrations/2016_03_17_112201_create_comments_table.php
+++ database/migrations/2016_03_17_112201_create_comments_table.php
@@ -16,6 +16,7 @@ class CreateCommentsTable extends Migration
             $table->increments('id');
             $table->integer('user_id')->unsigned();
             $table->integer('post_id')->unsigned();
+            $table->longText('commentaire');
             $table->timestamps();
         });
     }
