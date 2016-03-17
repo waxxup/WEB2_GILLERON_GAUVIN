78eda24a4d25470a3480cd9aee0249f65fc0643d
momolafripouille <Nicolas-g7@hotmail.fr>
2016-03-17 16:13:47 +0100

    avancée page profil

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,22 +28,6 @@ Route::get('/', function () {
 
 Route::group(['middleware' => ['web']], function () {
 
-    Route::group(['prefix' => 'articles'], function() {
-
-        /* Route::post('/articles', function(Request $request) {
-           dd($request->all());
-        }); */
-
-        /*  Route::post('/', [
-             'as' => 'articles.store',
-             'uses' => function (Request $request) {
-                 dd($request->all());
-             }]); */
-    });
-
-    Route::resource('/articles', 'PostController');
-
-
     Route::group(['prefix' => 'bap'], function() {
 
         /* Route::post('/articles', function(Request $request) {
@@ -77,9 +61,7 @@ Route::group(['middleware' => ['web']], function () {
     Route::resource('/bap', 'BapController');
 
     Route::resource('/admin', 'AdminController');
-
-
-
+    Route::get('/profil', 'UserProfilController@index' );
 
 });
 Route::get('/profil', function () {
