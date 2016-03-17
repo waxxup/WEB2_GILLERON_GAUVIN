f7c4f7b7057dc60fdf3c3e13c72fe23caf19efd5
momolafripouille <Nicolas-g7@hotmail.fr>
2016-03-17 12:50:56 +0100

    avancée page profil

---
+++ resources/views/profil/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+    <div class="panel-body">
+        <h1>LISTE DES PROJETS BAP VALIDES</h1>
+        <hr>
+
+        @foreach($users as $user)
+
+        @endforeach
+    </div>
+
+@endsection
